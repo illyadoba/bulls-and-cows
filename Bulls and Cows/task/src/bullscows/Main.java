a5d0d11da1293e1f6abb29350dfe60142932d9c6
ilja <illyadoba92@gmail.com>
2023-02-26 00:48:03 +0100

    task 2

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,7 +1,29 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String secretCode = "9305";
+        String guessCode = scanner.nextLine();
+        int cows = 0;
+        int bulls = 0;
+
+        for (int i = 0; i < secretCode.length(); i++) {
+            if (secretCode.charAt(i) == guessCode.charAt(i)) {
+                bulls++;
+            } else if (secretCode.contains(String.valueOf(guessCode.charAt(i)))) {
+                cows++;
+            }
+        }
+
+        if (cows == 0 && bulls == 0) {
+            System.out.println("Grade: None. The secret code is " + secretCode + ".");
+        } else if (bulls == 0) {
+            System.out.println("Grade: " + cows + " cow(s). The secret code is " + secretCode + ".");
+        } else {
+            System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s). The secret code is " + secretCode + ".");
+        }
     }
 }
